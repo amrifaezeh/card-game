2f5925ffb913bde7e942befda2941f3596c5713c
amrifaezeh <amrifaezeh@gmail.com>
2019-08-28 22:18:43 +0800

    updated index.js & app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+ 
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//         'div',
+//         null,
+//         'Welcome to React',
+//     )
+//   }
+// }
+ 
+// export default App;
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import propTypes from 'prop-types';
+import { isNumeric } from '../../Helper';
+import './playingcard.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const cardSuites = {
+  D: 'diamonds',
+  S: 'spades',
+  H: 'hearts',
+  C: 'clubs',
+};
 
-export default App;
+const cardCourts = {
+  K: 'king',
+  J: 'jack',
+  Q: 'queen',
+  A: 'ace',
+  T: '10',
+};
+
+const cardValues = {
+  ace: '14',
+  king: '13',
+  queen: '12',
+  jack: '11',
+};
+
+const PlayingCard = ({ cardSymbol }) => {
+  const url = convertCardSymbolToUrl(cardSymbol);
+  return <img alt="playing-card" className="playing-card" src={url} />;
+};
+
+PlayingCard.propTypes = { cardSymbol: propTypes.string.isRequired };
+
+export const convertCardSymbolToUrl = cardSymbol => `/cards/${getCardCourt(cardSymbol)}_of_${getCardSuite(cardSymbol)}.svg`;
+
+export const getCardSuite = cardSymbol => cardSuites[cardSymbol[1]];
+
+export const getCardCourt = cardSymbol => {
+  const courtSymbol = cardSymbol[0];
+  if (isNumeric(courtSymbol)) {
+    return String(courtSymbol);
+  }
+  return String(cardCourts[courtSymbol]);
+};
+
+export const getCardValue = cardCourt => {
+  if (isNumeric(cardCourt)) {
+    return Number(cardCourt);
+  }
+  return Number(cardValues[cardCourt]);
+};
+
+export default PlayingCard;
